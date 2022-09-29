c8e4b25383906cc7d2b547511ee83586599df868
Josh Felmeden <josh.felmeden@outlook.com>
2022-09-29 14:33:53 +0100

    completed xml and can write to any format (does not follow schema for json and xml)

--- main.py
+++ main.py
@@ -1,26 +1,51 @@
+from curses.ascii import isdigit
 import datetime
-from decimal import InvalidOperation
-import decimal
+from decimal import *
+import json
+#  decimal
 from multiprocessing.sharedctypes import Value
+import os
+from turtle import update
 from dateutil.parser import parse
 import pandas as pd
 import account
+import xml.etree.ElementTree as ET
 import transaction
 import logging
 
 logging.basicConfig(filename='supportbank.log',filemode='w',level=logging.DEBUG)
 
 # CONSTANTS {{{
-# TRANSACTIONS_FILE   = './transactions2014.csv'
-TRANSACTIONS_FILE   = './dodgytransactions2015.csv'
+TRANSACTIONS_FILE   = './data/transactions2012.xml'
+DATA_DIR            = './data'
+OUT_DIR             = './out'
+
 DEBUG               = False
 
 LIST_ALL            = 1
 LIST_NAME           = 2
-QUIT_MENU           = 3
+READ_FILE           = 3
+WRITE_FILE          = 4
+QUIT_MENU           = 5
 
 ID_RESPONSE         = 1
+
+OLE_TIME_ZERO       = datetime.datetime(1899, 12, 30, 0, 0, 0)
 # }}}
+
+def is_date(string:str, fuzzy=False):
+    try:
+        parse(string, fuzzy=fuzzy)
+        return True
+    except ValueError:
+        return False
+
+def is_num(string:str):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
 
 def create_accounts(transactions_df):
     accounts = {}
@@ -29,26 +54,19 @@ def create_accounts(transactions_df):
             accounts[str(id)] = account.Account(str(id))
     return accounts
 
-def is_date(string, fuzzy=False):
-    try:
-        parse(string, fuzzy=fuzzy)
-        return True
-    except ValueError:
-        return False
-
 def perform_transactions(transactions_df, accounts):
     line_no = 0
     for t in transactions_df.iloc:
         try:
             new_transaction = transaction.Transaction(t['Date'],t['From'],t['To'],t['Narrative'],t['Amount'])
-            if not (is_date(t['Date'])):
+            if not (is_date(str(t['Date']))):
                 raise ValueError()
 
             accounts[t['From']].transaction_history.append(new_transaction)
             accounts[t['To']].transaction_history.append(new_transaction)
 
-            accounts[t['From']].balance -= decimal.Decimal(t['Amount'])
-            accounts[t['To']].balance   += decimal.Decimal(t['Amount'])
+            accounts[t['From']].balance -= int(t['Amount'])
+            accounts[t['To']].balance   += int(t['Amount'])
         except InvalidOperation:
             logging.error(f'{curr_time()}: Invalid amount on line {line_no}')
         except ValueError:
@@ -56,10 +74,69 @@ def perform_transactions(transactions_df, accounts):
         line_no += 1
     return accounts
 
-def pre_process():
-    transactions_df = pd.read_csv(TRANSACTIONS_FILE)
-    logging.info(f'{curr_time()}: Read csv {TRANSACTIONS_FILE}')
-    transactions_df['Amount']
+def read_json(curr_file):
+    new_headers = {'date':'Date','fromAccount':'From','toAccount':'To','narrative':'Narrative','amount':'Amount'}
+    transactions_df = pd.DataFrame(pd.read_json(curr_file))
+    transactions_df.rename(columns=new_headers,inplace=True)
+    return transactions_df
+
+def read_xml(curr_file):
+    transactions_xml = ET.parse(curr_file)
+    root = transactions_xml.getroot()
+    column_headers = ['Date', 'From', 'To', 'Narrative', 'Amount']
+    # creating a dataframe from a list of lists is much more efficient than creating a DF line by line
+    list_transactions = []
+    for transaction in root.findall('SupportTransaction'):
+        t_narrative = transaction.find('Description').text
+        t_date = datetime.datetime.strftime(OLE_TIME_ZERO + datetime.timedelta(days=int(transaction.get('Date'))), '%d/%m/%Y')
+        t_amount = transaction.find('Value').text
+        t_from = transaction.find('Parties').find('From').text
+        t_to = transaction.find('Parties').find('To').text
+        list_transactions.append([t_date, t_from, t_to, t_narrative, t_amount])
+    transactions_df = pd.DataFrame(list_transactions, columns=column_headers)
+    return transactions_df
+
+def write_file(file_name, curr_file):
+    new_file_path = os.path.join(OUT_DIR, file_name)
+    transactions_df = read_file(curr_file)
+    print('Writing to ' + str(new_file_path))
+    if file_name.endswith('.csv'):
+        transactions_df.to_csv(new_file_path, index=False)
+        print('Write successful')
+        logging.info(f'{curr_time()}: Successful write to file {new_file_path}')
+    elif file_name.endswith('.json'):
+        result = transactions_df.to_json(orient='records')
+        parsed = json.loads(result)
+        with open(new_file_path, 'w') as f:
+            json.dump(parsed, f, indent=4) 
+    elif file_name.endswith('xml'):
+        transactions_df.to_xml(new_file_path, index=False)
+
+def read_file(curr_file):
+    if curr_file.lower().endswith('.csv'):
+        logging.info(f'{curr_time()} filetype is csv')
+        transactions_df = pd.read_csv(curr_file)
+    elif curr_file.lower().endswith('.json'):
+        logging.info(f'{curr_time()} filetype is json')
+        transactions_df = read_json(curr_file)
+    elif curr_file.lower().endswith('.xml'):
+        logging.info(f'{curr_time()} filetype is xml')
+        transactions_df = read_xml(curr_file)
+    logging.info(f'{curr_time()}: Read file {curr_file}')
+    return transactions_df
+
+def pre_process(curr_file):
+    transactions_df = read_file(curr_file)
+
+    line_no = 0
+    for amount in transactions_df['Amount']:
+        if not (is_num(amount)):
+            logging.warning(f'{curr_time()}: "{amount}" is not a numeric value at line {line_no}, transaction removed')
+            transactions_df = transactions_df.drop(index=line_no)
+        else:
+            transactions_df.at[line_no, 'Amount'] = float(transactions_df['Amount'][line_no]) * 100
+        line_no += 1
+
     if DEBUG:
         print(transactions_df['Amount'])
         print(transactions_df.iloc[0])
@@ -71,59 +148,97 @@ def pre_process():
     return perform_transactions(transactions_df, accounts)
 
 def print_menu():
-    print('========================MAIN MENU===============================')
-    print('Option    Alternate    Description')
-    print('1)        List All     Output name and balance of each person')
-    print('2 [Name]) List [Name]  Output transaction history for *NAME*')
-    print('3)        Quit         Quit program')
-    print('================================================================')
+    COL_WIDTH = 90
+    TOP_BAR_WIDTH = int(COL_WIDTH/2 - 5)
+    print('='*TOP_BAR_WIDTH + 'MAIN  MENU' + '='*TOP_BAR_WIDTH)
+    print('{0:15} {1:15} {2:40}'.format('OPTION', 'ALTERNATE','DESCRIPTION'))
+    print('{0:15} {1:15} {2:40}'.format('1', 'List All','Output name and balance of each account'))
+    print('{0:15} {1:15} {2:40}'.format('2 [Name]', 'List [Name]','Output transaction history for *NAME*'))
+    print('{0:15} {1:15} {2:40}'.format('3 [Filename]', 'Read [Filename]','Import file from disk (must be in "data" directory)'))
+    print('{0:15} {1:15} {2:40}'.format('4 [Filename]', 'Write [Filename]','Export file to disk (will be written to "out" directory)'))
+    print('{0:15} {1:15} {2:40}'.format('5', 'Quit','Exit program'))
+    print('='*COL_WIDTH)
 
 def menu_choice():
     selection = ''
     id = ''
+    file_name = ''
     while selection == '':
         response = input('Select menu option: ')
-        if response.lower() in ['1', 'list all']:
+        if response.lower() in [str(LIST_ALL), 'list all']:
             selection = LIST_ALL
-        elif response.lower()[4:] not in ['2','list'] and len(response.split()) >= 2:
+        elif response.split()[0] in [str(LIST_NAME),'list'] and len(response.split()) >= 2:
             selection = LIST_NAME
             id = " ".join(response.split()[ID_RESPONSE:])
-        elif response.lower() in ['3', 'quit', 'exit']:
+        elif response.split()[0] in [str(READ_FILE), 'read'] and len(response.split()) >= 2:
+            selection = READ_FILE
+            file_name = response.split()[1]
+        elif response.split()[0] in [str(WRITE_FILE), 'write'] and len(response.split()) >= 2:
+            selection = WRITE_FILE
+            file_name = response.split()[1]
+        elif response.lower() in [str(QUIT_MENU), 'quit', 'exit']:
             selection = QUIT_MENU
-    return (selection,id)
+    return (selection,id,file_name)
 
 def list_all_entries(updated_accounts):
+    print('{0:15} {1:>7}'.format('ACCOUNT NAME', 'BALANCE'))
     for id in updated_accounts:
-        print(f'{id} £{decimal.Decimal(updated_accounts[id].balance)}')
+        print('{0:15} {1:>7}'.format(id, '£' + str(round(updated_accounts[id].balance/100,2))))
 
 def list_transaction_history(updated_accounts, id):
+    print(f'Transaction history for {id}')
+    print(transaction.Transaction.PRINT_FORMAT.format('DATE', 'FROM', 'TO', 'NARRATIVE', 'AMOUNT'))
     if id in updated_accounts:
         for t in updated_accounts[id].transaction_history:
             t.print_transaction()
     else:
+        logging.warning(f'{curr_time()}: {id} not present in selected file')
         print(f'Account name "{id}" not recognised')
 
 def curr_time():
     return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 def main():
-    logging.info(f'{curr_time()} Execution finished')
-    updated_accounts = pre_process()
-    selection = ''
-    while selection != QUIT_MENU:
-        print_menu()
-        logging.info(f'{curr_time()}: Displaying menu')
-        (selection,id) = menu_choice()
-        if DEBUG:
-            print(id)
-        if selection == LIST_ALL:
-            logging.info(f'{curr_time()}: List all selected')
-            list_all_entries(updated_accounts)
-        elif selection == LIST_NAME:
-            logging.info(f'{curr_time()}: List {id} selected')
-            list_transaction_history(updated_accounts, id)
-        elif selection != QUIT_MENU:
-            logging.info(f'{curr_time()}: Invalid menu option selected')
+    logging.info(f'{curr_time()} Execution started')
+    curr_file = TRANSACTIONS_FILE
+    if (curr_file.lower().endswith(('.json', '.csv', '.xml'))):
+        updated_accounts = pre_process(curr_file)
+        selection = ''
+        while selection != QUIT_MENU:
+            print_menu()
+            logging.info(f'{curr_time()}: Displaying menu')
+            (selection,id,file_name) = menu_choice()
+            if DEBUG:
+                print(id)
+            if selection == LIST_ALL:
+                logging.info(f'{curr_time()}: List all selected')
+                list_all_entries(updated_accounts)
+            elif selection == LIST_NAME:
+                logging.info(f'{curr_time()}: List {id} selected')
+                list_transaction_history(updated_accounts, id)
+            elif selection == READ_FILE:
+                logging.info(f'{curr_time()}: Read file {file_name} selected')
+                new_file_path = os.path.join(DATA_DIR, file_name)
+                if os.path.isfile(new_file_path):
+                    logging.info(f'{curr_time()}: Changing information to file {new_file_path}')
+                    curr_file = new_file_path
+                    updated_accounts = pre_process(curr_file)
+                else:
+                    print('File does not exist')
+                    logging.warning(f'{curr_time()}: Requested file {new_file_path} does not exist')
+            elif selection == WRITE_FILE:
+                if file_name.endswith(('.csv', '.xml', '.json')):
+                    write_file(file_name, curr_file)
+                else:
+                    print('That file format is not supported')
+                    logging.warning(f'{curr_time}: File {file_name} not supported')
+            elif selection != QUIT_MENU:
+                logging.info(f'{curr_time()}: Invalid menu option selected')
+            if selection != QUIT_MENU:
+                input('Press enter to continue...')
+                os.system('cls' if os.name == 'nt' else 'clear')
+    else:
+        logging.error(f'{curr_time()}: Invalid file format for transaction file')
     logging.info(f'{curr_time()}: Execution finished')
 
 if __name__ == '__main__':
--- transaction.py
+++ transaction.py
@@ -2,6 +2,7 @@ from datetime import date
 import decimal
 
 class Transaction:
+    PRINT_FORMAT = '{0:10} {1:10} {2:10} {3:30} {4:>6}'
     def __init__(self, date='', from_id='', to_id='', narrative='',amount=0) -> None:
         self.date        = date
         self.from_id     = from_id
@@ -10,4 +11,5 @@ class Transaction:
         self.amount      = amount
 
     def print_transaction(self):
-        print(f'date:{self.date}, from:{self.from_id}, to:{self.to_id}, narrative: {self.narrative}, amount:£{decimal.Decimal(self.amount)}')
+        # print(f'date:{self.date}, from:{self.from_id}, to:{self.to_id}, narrative: {self.narrative}, amount:£{self.amount/100}')
+        print(self.PRINT_FORMAT.format(self.date, self.from_id, self.to_id, self.narrative, '£' + str(round(self.amount/100, 2))))
